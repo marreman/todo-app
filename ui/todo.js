b48e6a4c2754ff3bd104298ae053fa787af13f01
Martin Liberg <mr.liberg@gmail.com>
2014-03-23 13:31:06 +0100

    firebase implementation

--- ui/todo.js
+++ ui/todo.js
@@ -4,69 +4,83 @@ app(function () {
     var tpl = app.tpl('item'),
         list = app.el.find('.list');
 
-    function renderItem(item) {
-        var node = $($.render(tpl, { name: item }));
+    function Item(data, meta) {
+        var _this = this;
 
-        node.find('.item-remove').on('click', function (event) {
+        this.data = data;
+        this.meta = meta;
+        this.el = $($.render(tpl, this.data));
+
+        this.el.find('.item-remove').on('click', function (event) {
             event.preventDefault();
-            //node.remove();
-            app.trigger('item:remove', item);
+            meta.trigger('remove');
         });
 
-        node.find('.item-checkbox').on('change', function () {
-            node[(this.checked ? 'addClass' : 'removeClass')]('item-done');
+        this.el.find('.item-checkbox').prop('checked', this.data.done).on('change', function () {
+            _this.data.done = this.checked;
+            _this.el[(_this.data.done ? 'addClass' : 'removeClass')]('item-done');
+            meta.trigger('change');
         });
 
-        list.append(node);
+        this.meta.on('removed', function () {
+            _this.el.remove();
+        });
     }
 
-    app.on('items', function (items) {
-        list.empty();
-        if (items) {
-            items.forEach(renderItem);
-        }
-    })
+    app.on('item:added', function (data, meta) {
+        var item = new Item(data, meta);
+        list.append(item.el);
+    });
 });
 
 app(function () {
-    "use strict";
+    'use strict';
 
-    var loc = window.location;
+    var uuid = window.location.hash = window.location.hash.slice(1) || uuid(),
+        fb = new Firebase('https://to-buy.firebaseio.com/').child(uuid),
+        items = {};
 
-    function clean(items) {
-        return items.map(function (item) {
-            return $.trim(item);
-        }).filter(function (item) {
-            return !!item;
+    // function Model(snap) {
+    //     this.data =
+    // }
+
+    function uuid() {
+        return ('0000' + (Math.random()*Math.pow(36,4) << 0).toString(36)).substr(-4);
+    }
+
+    fb.on('child_added', function (snap) {
+        var item = snap.val(),
+            meta = $.observable({ id: snap.name() });
+
+        meta.on('change', function () {
+            fb.child(meta.id).set(item);
         });
-    }
 
-    function hash(val) {
-        if (val) {
-            loc.hash = clean(val).join(',');
-        } else {
-            return clean(loc.hash.slice(1).split(','));
-        }
-    }
+        meta.on('remove', function () {
+            fb.child(meta.id).remove();
+        });
 
-    $.route(function () {
-        app.trigger('items', hash());
+        items[meta.id] = {
+            data: item,
+            meta: meta
+        };
+
+        app.trigger('item:added', item, meta);
     });
 
-    app.on('item:new', function (item) {
-        var items = hash();
-        items.push($.trim(item));
-        hash(items);
+    fb.on('child_removed', function(snap) {
+        var item = items[snap.name()];
+        item.meta.trigger('removed');
     });
 
-    app.on('item:remove', function (item) {
-        var items = hash(),
-            index = items.indexOf(item);
-
-        items.splice(index, 1);
-        hash(items);
+    app.on('item:new', function (itemName) {
+        fb.push({
+            name: itemName,
+            done: false
+        });
     });
 });
+
 
 app(function () {
     "use strict";
